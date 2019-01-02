3c4301f74d26cc06957d851b7d511a1e0a3cb28e
quakex <iquakex@gmail.com>
2019-01-02 14:17:15 +0800

    osxdev-day01_02

---
+++ day01/day01/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  day01
+//
+//  Created by Xu Ling on 2019/1/1.
+//  Copyright © 2019 bitxcat. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
